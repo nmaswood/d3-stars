b06f48b4f6b1311830ce95b5cd1f38c74a588dd1
Nasr Maswood <nmaswood@uchicago.edu>
2017-12-06 02:10:28 -0600

    Website pretty much finished

--- js/index.js
+++ js/index.js
@@ -6,14 +6,18 @@ class SVG {
         this.padding = padding;
     }
 
-    createSVG(){
+    createSVGContainer(){
         const svgContainer = d3.select('#entry')
             .append('div')
             .attr('id', 'svg-container')
             .style('width', '100%')//(this.maxWidth + this.padding) + 'px')
             .style('height', '100%')//(this.maxHeight + this.padding) + 'px');
+        return svgContainer;
+    }
 
-        const svg = d3.select('#svg-container')
+    createSVG(svgContainer){
+
+        const svg = svgContainer 
             .append('svg')
             .attr('class', 'main-svg')
             .attr('width', '100%')//this.maxWidth)
@@ -72,8 +76,6 @@ class SVG {
         return `translate(${point.x} ${point.y}) rotate(${point.rotationAngle}) scale(${point.size})`
     }
 
-
-
     createStars(svg,data){
         const that = this;
 
@@ -93,8 +95,8 @@ class SVG {
             .data(data)
             .enter()
             .append('g')
-            .call(d3.drag().on("drag", dragged))
-           .attr('transform', function(d) { return that.createTransform(d)})
+            //.call(d3.drag().on("drag", dragged))
+            .attr('transform', function(d) { return that.createTransform(d)})
             .append('path')
             .attr('d', this.createStarAttr())
             .attr('stroke', 'yellow')
@@ -105,17 +107,52 @@ class SVG {
             .delay(40)
             .on("start", function repeat() {
                 d3.active(this)
-                    .attr('stroke-width', strokeWidth * 3)
+                    .attr('stroke-width', strokeWidth * 5)
                     .transition()
                     .duration(function(d) {return d.transitionDuration})
                     .attr('stroke-width', strokeWidth)
                     .transition()
                     .on("start", repeat);
-            })
-            
+            });
+        return stars;
+    }
 
+    createText(svg) {
+        const div = svg.append('g')
+            .append('text')
+            .attr('x', this.maxWidth / 2)
+            .attr('y', this.maxHeight / 3.2)
+            .attr('font-family', 'Verdana')
+            .attr('font-size', '30px')
+            .attr('fill', 'white')
+            .append("tspan")
+            .text("Hi. My name is Nasr Maswood.")
+            .attr('dx', -220)
+            .append("tspan")
+            .text("I am a 22 year old Software Engineer based in Chicago.")
+            .attr('dx', -650)
+            .attr('dy', 50)
+            .append("tspan")
+            .text("I enjoy Sufi Poetry, freestyle rapping and most recently ballroom dance.")
+            .attr('dx', -650 - 290)
+            .attr('dy', 50)
+            .append("tspan")
+            .text("Feel free to reach out at nasrmaswood@gmail.com")
+            .attr('dx', -650 - 150)
+            .attr('dy', 50)
+            .attr('font-size', '15px')
+            .append('tspan')
+            .text("To see my old website click here!")
+            .attr('dx', -320)
+            .attr('dy', 50)
+            .attr('font-size', '15px')
+            .style('cursor', 'pointer')
+            .on('click',
+                function(){
+                    window.open("http://google.com")
 
-
+                })
+            .style("text-anchor", "middle");
     }
 }
 
@@ -127,9 +164,11 @@ class Enter {
     main(){
         const data = GenerateData.generate(this.c.dataPoints, this.c.maxWidth, this.c.maxHeight, this.c.maxStarSize);
         const SVGClass = new SVG(data, this.c.maxHeight, this.c.maxWidth, this.c.padding);
-        const svg = SVGClass.createSVG();
+        const svgContainer = SVGClass.createSVGContainer();
+        const svg = SVGClass.createSVG(svgContainer);
         const background = SVGClass.createBackground(svg);
         const stars = SVGClass.createStars(svg, data);
+        const textBox = SVGClass.createText(svg);
 
     }
 }
